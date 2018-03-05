f6b45d5bb8a7bde71e44994c62068f4307b59a27
Manuel Benz <manuelf.benz@gmail.com>
2018-03-05 18:05:53 +0100

    Found a (propably super inefficient solution) to make the previous algorithm work as a heuristic.

--- src/main/java/de/upb/testify/efsm/eefsm/EEFSMFPAlgo.java
+++ src/main/java/de/upb/testify/efsm/eefsm/EEFSMFPAlgo.java
@@ -6,12 +6,14 @@ import de.upb.testify.efsm.Configuration;
 import de.upb.testify.efsm.EFSMPath;
 import de.upb.testify.efsm.JGraphBasedFPALgo;
 import org.jgrapht.GraphPath;
-import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
-import org.jgrapht.alg.shortestpath.FloydWarshallShortestPaths;
+import org.jgrapht.alg.interfaces.KShortestPathAlgorithm;
+import org.jgrapht.alg.shortestpath.KShortestPaths;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Collectors;
 
 /**
  * @author Manuel Benz
@@ -19,15 +21,17 @@ import java.util.ListIterator;
  */
 public class EEFSMFPAlgo<State, Input, ContextObject> extends JGraphBasedFPALgo<State, Input, EEFSMContext<ContextObject>, ETransition<State, Input, ContextObject>> {
 
-  private final ShortestPathAlgorithm<State, ETransition<State, Input, ContextObject>> shortestPaths;
+  private final KShortestPathAlgorithm<State, ETransition<State, Input, ContextObject>> shortestPaths;
   private final EEFSM<State, Input, ContextObject> eefsm;
   private final Multimap<ContextObject, ETransition<State, Input, ContextObject>> contextToAdders;
   private final Multimap<ContextObject, ETransition<State, Input, ContextObject>> contextToRemovers;
 
+  private final int K = 5;
+
   public EEFSMFPAlgo(EEFSM<State, Input, ContextObject> eefsm) {
     super(eefsm);
     this.eefsm = eefsm;
-    shortestPaths = new FloydWarshallShortestPaths<>(this.baseGraph);
+    shortestPaths = new KShortestPaths(this.baseGraph, K);
     contextToAdders = MultimapBuilder.hashKeys().arrayListValues().build();
     contextToRemovers = MultimapBuilder.hashKeys().arrayListValues().build();
 
@@ -53,62 +57,69 @@ public class EEFSMFPAlgo<State, Input, ContextObject> extends JGraphBasedFPALgo<
 
   @Override
   public EEFSMPath<State, Input, ContextObject> getPath(Configuration<State, EEFSMContext<ContextObject>> config, State tgt) {
-    // check if there is any path first
-    GraphPath<State, ETransition<State, Input, ContextObject>> path = shortestPaths.getPath(config.getState(), tgt);
-    if (path == null) {
+    List<GraphPath<State, ETransition<State, Input, ContextObject>>> paths = shortestPaths.getPaths(config.getState(), tgt);
+    if (paths.isEmpty()) {
       return null;
     }
 
-    return backTrack(config.getContext(), new EEFSMPath(eefsm, path), 0);
+    return backTrack(config.getContext(), toEFSMPath(paths), 0);
   }
 
+  private List<EEFSMPath<State, Input, ContextObject>> toEFSMPath(List<GraphPath<State, ETransition<State, Input, ContextObject>>> paths) {
+    return paths.stream().map(p -> new EEFSMPath<>(eefsm, p)).collect(Collectors.toList());
+  }
 
   /**
    * @param context
-   * @param curSolution
-   * @param index       Count of transitions that were already traversed and for which the domain guards are already satisfied in the current path (counting from the end of the path)
+   * @param possibleSolutions
+   * @param depth             Count of transitions that were already traversed and for which the domain guards are already satisfied in the current path (counting from the end of the path)
    * @return
    */
-  private EEFSMPath<State, Input, ContextObject> backTrack(EEFSMContext<ContextObject> context, EEFSMPath<State, Input, ContextObject> curSolution, int index) {
-    if (curSolution.isFeasible(context)) {
-      return curSolution;
+  private EEFSMPath<State, Input, ContextObject> backTrack(EEFSMContext<ContextObject> context, List<EEFSMPath<State, Input, ContextObject>> possibleSolutions, int depth) {
+    for (EEFSMPath<State, Input, ContextObject> solution : possibleSolutions) {
+      if (solution.isFeasible(context)) {
+        return solution;
+      }
     }
     // we do not want to accept complete loops since this means the path is not satisfiable
 
+    for (EEFSMPath<State, Input, ContextObject> possibleSolution : possibleSolutions) {
+      // we start at a point in the path that
+      List<ETransition<State, Input, ContextObject>> transitions = possibleSolution.getTransitions();
 
-    // we start at a point in the path that
-    List<ETransition<State, Input, ContextObject>> transitions = curSolution.getTransitions();
-    ListIterator<ETransition<State, Input, ContextObject>> reverseIter = transitions.listIterator(transitions.size() - index);
+      int solutionDepth = depth;
+      ListIterator<ETransition<State, Input, ContextObject>> reverseIter = transitions.listIterator(transitions.size() - depth);
 
-    while (reverseIter.hasPrevious()) {
-      ETransition<State, Input, ContextObject> previous = reverseIter.previous();
-      index++;
+      while (reverseIter.hasPrevious()) {
+        ETransition<State, Input, ContextObject> previous = reverseIter.previous();
+        solutionDepth++;
 
-      if (previous.hasDomainGuard()) {
-        ContextObject expectedContext = previous.getExpectedContext();
-        Collection<ETransition<State, Input, ContextObject>> solvers;
-        if (previous.isElementOfGuard()) {
-          solvers = contextToAdders.get(expectedContext);
-        } else {
-          solvers = contextToRemovers.get(expectedContext);
-        }
-
-        if (solvers == null) {
-          // FIXME we have to handle this correctly
-          throw new IllegalStateException("Not satisfiable");
-        }
-
-        for (ETransition<State, Input, ContextObject> intermediate : solvers) {
-          EEFSMPath<State, Input, ContextObject> result;
-          // if the intermediate we need to take is already in the not-yet evaluated sub-path, we do not add it again
-          if (curSolution.subPath(0, index).contains(intermediate)) {
-            result = backTrack(context, curSolution, index);
+        if (previous.hasDomainGuard()) {
+          ContextObject expectedContext = previous.getExpectedContext();
+          Collection<ETransition<State, Input, ContextObject>> solvers;
+          if (previous.isElementOfGuard()) {
+            solvers = contextToAdders.get(expectedContext);
           } else {
-            result = backTrack(context, pathOverIntermediate(curSolution, index, intermediate), index);
+            solvers = contextToRemovers.get(expectedContext);
           }
 
-          if (result != null) {
-            return result;
+          if (solvers == null) {
+            // FIXME we have to handle this correctly
+            throw new IllegalStateException("Not satisfiable");
+          }
+
+          for (ETransition<State, Input, ContextObject> intermediate : solvers) {
+            EEFSMPath<State, Input, ContextObject> result;
+            // if the intermediate we need to take is already in the not-yet evaluated sub-path, we do not add it again
+            if (possibleSolution.subPath(0, solutionDepth).contains(intermediate)) {
+              result = backTrack(context, possibleSolutions, solutionDepth);
+            } else {
+              result = backTrack(context, k2PathsOverIntermediate(possibleSolution, solutionDepth, intermediate), solutionDepth);
+            }
+
+            if (result != null) {
+              return result;
+            }
           }
         }
       }
@@ -117,30 +128,31 @@ public class EEFSMFPAlgo<State, Input, ContextObject> extends JGraphBasedFPALgo<
     return null;
   }
 
-  private EEFSMPath<State, Input, ContextObject> pathOverIntermediate(EEFSMPath<State, Input, ContextObject> curSolution, int index, ETransition<State, ?, ?> intermediate) {
+  private List<EEFSMPath<State, Input, ContextObject>> k2PathsOverIntermediate(EEFSMPath<State, Input, ContextObject> curSolution, int index, ETransition<State, ?, ?> intermediate) {
     State intermediateSrc = intermediate.getSrc();
     State intermediateTgt = intermediate.getTgt();
 
     int curLength = curSolution.getLength();
     int curIndex = curLength - index;
 
-    GraphPath intermediateToCur = shortestPaths.getPath(intermediateTgt, curSolution.getTransitionAt(curIndex).getSrc());
+    List<GraphPath<State, ETransition<State, Input, ContextObject>>> intermediateToCurs = shortestPaths.getPaths(intermediateTgt, curSolution.getTransitionAt(curIndex).getSrc());
+    EFSMPath curToTarget = curSolution.subPath(curIndex, curLength);
 
-    if (intermediateToCur != null) {
-      GraphPath srcToIntermediate = shortestPaths.getPath(curSolution.getSrc(), intermediateSrc);
+    List<EEFSMPath<State, Input, ContextObject>> res = new ArrayList<>();
 
-      if (srcToIntermediate != null) {
+    for (GraphPath<State, ETransition<State, Input, ContextObject>> intermediateToCur : intermediateToCurs) {
+      List<GraphPath<State, ETransition<State, Input, ContextObject>>> srcToIntermediates = shortestPaths.getPaths(curSolution.getSrc(), intermediateSrc);
+
+      for (GraphPath<State, ETransition<State, Input, ContextObject>> srcToIntermediate : srcToIntermediates) {
         // glue all the sub-parts together
         EEFSMPath<State, Input, ContextObject> newSolution = new EEFSMPath(eefsm, srcToIntermediate);
         newSolution.append(intermediate);
         newSolution.append(intermediateToCur);
-
-        EFSMPath curToTarget = curSolution.subPath(curIndex, curLength);
         newSolution.append(curToTarget);
-
-        return newSolution;
+        res.add(newSolution);
       }
     }
-    return null;
+
+    return res;
   }
 }
