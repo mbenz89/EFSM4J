c685a6ab1a5c6005273dc9b38a7a107272fb8b39
Manuel Benz <manuelf.benz@gmail.com>
2018-03-27 17:37:02 +0200

    fixed path of encrypted key

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_89f01b983058_key -iv $encrypted_89f01b983058_iv -in codesigning.asc.enc -out codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_89f01b983058_key -iv $encrypted_89f01b983058_iv -in codesigning.asc.enc -out cd/codesigning.asc -d
 	gpg --fast-import cd/signingkey.asc
 fi
