3bf394d6dad06b179e48ba68d5d38e3da8b4c4ce
Manuel Benz <manuelf.benz@gmail.com>
2018-05-24 13:45:57 +0200

    Improved bounds for generics in IEFSMContext

--- src/main/java/de/upb/testify/efsm/IEFSMContext.java
+++ src/main/java/de/upb/testify/efsm/IEFSMContext.java
@@ -1,7 +1,7 @@
 package de.upb.testify.efsm;
 
 /** @author Manuel Benz created on 26.02.18 */
-public interface IEFSMContext<Context> {
+public interface IEFSMContext<Context extends IEFSMContext> {
 
   /**
    * Creates a hard copy of this context which can live and change independently
